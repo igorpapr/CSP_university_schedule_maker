723806d2e26d200545ef86bdf2e690de6fce9ccf
Ihor Paprotskyi <igorpapr@gmail.com>
2020-11-01 23:10:12 +0200

    added constructor to mrvforwardchecker

--- src/entity/solver/MRVForwardCheckingSolver.java
+++ src/entity/solver/MRVForwardCheckingSolver.java
@@ -2,13 +2,23 @@ package entity.solver;
 
 import entity.ScheduleSlot;
 import entity.UniversityClass;
+import entity.constraints.AbstractConstraint;
+
+import java.util.List;
 
 //Minimum remaining values solver with forward checking
 public class MRVForwardCheckingSolver extends ScheduleSolver{
 
+	public MRVForwardCheckingSolver(List<UniversityClass> variables, List<AbstractConstraint> constraints) {
+		this.variables = variables;
+		this.constraints = constraints;
+	}
+
 	@Override
 	public void solve() {
-
+		for (int i = 0; i < variables.size(); i++){
+			//tbd
+		}
 	}
 
 	@Override
