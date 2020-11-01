312bd63fcb96884c60c6850781943f88c9e68beb
Ihor Paprotskyi <igorpapr@gmail.com>
2020-11-01 22:55:34 +0200

    Fixed solver

--- src/ScheduleSolver.java
+++ src/ScheduleSolver.java
@@ -1,4 +1,5 @@
 import entity.UniversityClass;
+import entity.constraints.AbstractConstraint;
 
 import java.util.List;
 
@@ -6,13 +7,13 @@ public abstract class ScheduleSolver {
 
 	protected List<UniversityClass> variables;
 
+	protected List<AbstractConstraint> constraints;
+
 	//solve the schedule
 	public abstract void solve();
 
 	//heuristics to assign the value to the variable
-	protected abstract void abstractValue(UniversityClass variable);
+	protected abstract void assignValue(UniversityClass variable);
 
-
-
-
+	//TODO maybe add returning mechanism
 }
